acf2e885f84ac41473632e51bb194db86ea2f2e0
David <david_why@outlook.com>
2021-06-20 13:36:43 +0800

    feat: Add homework #2

---
+++ cpp-lvl3/006-search-review/hw/002-ksum.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, k;
+int nums[100];
+set<int> ans;
+
+void dfs(int prev, int sum, int need)
+{
+    if (need == 0)
+    {
+        ans.insert(sum);
+        return;
+    }
+
+    need--;
+    for (int i = prev + 1; i < n; i++)
+    {
+        dfs(i, sum + nums[i], need);
+    }
+}
+
+int main()
+{
+    cin >> n >> k;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> nums[i];
+    }
+
+    sort(nums, nums + n);
+    dfs(-1, 0, k);
+
+    cout << ans.size() << "\n";
+    int s = ans.size();
+    for (int i : ans)
+    {
+        s--;
+        cout << i << (s ? " " : "\n");
+    }
+
+    return 0;
+}
