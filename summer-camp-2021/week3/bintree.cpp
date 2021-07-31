08e1d60a439eaaaff71b92129d38b2f60009f035
David <david_why@outlook.com>
2021-07-31 21:06:38 +0800

    fix: Simplify question bintree

--- summer-camp-2021/week3/bintree.cpp
+++ summer-camp-2021/week3/bintree.cpp
@@ -1,44 +1,39 @@
 #include <fstream> // ifstream, ofstream
+#include <map>     // map
+#include <utility> // pair, make_pair
 
 using namespace std;
 
-struct node
-{
-    char val;
-    char left;
-    char right;
-} nodes[26];
+map<char, pair<char, char>> nodes;
 int n;
 
-void show(const node &n, ofstream &fout)
+void show(char c, ofstream &fout)
 {
-    fout << n.val;
-    if (n.left != '*')
-        show(nodes[n.left - 'a'], fout);
-    if (n.right != '*')
-        show(nodes[n.right - 'a'], fout);
+    if (c == '*')
+        return;
+
+    fout << c;
+    show(nodes[c].first, fout);
+    show(nodes[c].second, fout);
 }
 
 int main()
 {
     ifstream fin("bintree.in");
     fin >> n;
-    int root = -1;
+    char root = 0;
     for (int i = 0; i < n; i++)
     {
         char val, left, right;
         fin >> val >> left >> right;
-        int index = val - 'a';
-        if (root == -1)
-            root = index;
-        nodes[index].val = val;
-        nodes[index].left = left;
-        nodes[index].right = right;
+        if (root == 0)
+            root = val;
+        nodes[val] = make_pair(left, right);
     }
     fin.close();
 
     ofstream fout("bintree.out");
-    show(nodes[root], fout);
+    show(root, fout);
     fout.close();
 
     return 0;
