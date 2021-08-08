1076ed3203f9a190deca495b41c76d22cfe00b07
David <david_why@outlook.com>
2021-08-08 12:51:19 +0800

    fix: Fix question buy

--- summer-camp-2021/week4/buy.cpp
+++ summer-camp-2021/week4/buy.cpp
@@ -6,12 +6,10 @@ using namespace std;
 /**
  * Index 0: Maximum item ID
  * Index 1: Amount of money
- * Value:   Maximum 'importance'
+ * Value:   Maximum sum of importance * price
  */
 int dp[26][30001];
-int answers[26][30001];
-// {importance, price}
-pair<int, int> items[25];
+int importance[26], prices[26];
 
 int main()
 {
@@ -19,31 +17,22 @@ int main()
     int n, m;
     fin >> n >> m;
     for (int i = 1; i <= m; i++)
-        fin >> items[i].second >> items[i].first;
+        fin >> prices[i] >> importance[i];
     fin.close();
 
-    int ans = 0;
     for (int i = 1; i <= m; i++)
     {
         for (int j = 1; j <= n; j++)
         {
-            if (j >= items[i].second && (j == items[i].second || dp[i][j - items[i].second] != dp[i][j - items[i].second - 1]) && dp[i - 1][j] < dp[i - 1][j - items[i].second] + items[i].first)
-            {
-                dp[i][j] = dp[i - 1][j - items[i].second] + items[i].first;
-                answers[i][j] = answers[i - 1][j - items[i].second] + items[i].first * items[i].second;
-            }
+            if (j >= prices[i] && dp[i - 1][j - prices[i]] + importance[i] * prices[i] > dp[i - 1][j])
+                dp[i][j] = dp[i - 1][j - prices[i]] + importance[i] * prices[i];
             else
-            {
                 dp[i][j] = dp[i - 1][j];
-                answers[i][j] = answers[i - 1][j];
-            }
-            if (answers[i][j] > ans)
-                ans = answers[i][j];
         }
     }
 
     ofstream fout("buy.out");
-    fout << ans << endl;
+    fout << dp[m][n] << endl;
     fout.close();
 
     return 0;
