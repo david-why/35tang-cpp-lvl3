492573fb45efd7ca14c6bd4e02ce6584590109cc
David <david_why@outlook.com>
2021-09-04 19:33:29 +0800

    fix: Fix homework #2

--- cpp-lvl4/003-tree/hw/002-isbst.cpp
+++ cpp-lvl4/003-tree/hw/002-isbst.cpp
@@ -7,14 +7,16 @@ struct node
     int left, right, val;
 } nodes[10001];
 
-bool dfs(int i)
+bool dfs(int i, int min, int max)
 {
     if (!i)
         return true;
     node &n = nodes[i];
+    if (n.val < min || n.val > max)
+        return false;
     if ((n.left && n.val <= nodes[n.left].val) || (n.right && n.val >= nodes[n.right].val))
         return false;
-    return dfs(n.left) && dfs(n.right);
+    return dfs(n.left, min, n.val) && dfs(n.right, n.val, max);
 }
 
 int main()
@@ -24,7 +26,7 @@ int main()
     for (int i = 1; i <= n; i++)
         cin >> nodes[i].val >> nodes[i].left >> nodes[i].right;
 
-    cout << dfs(1) << endl;
+    cout << dfs(1, INT_MIN, INT_MAX) << endl;
 
     return 0;
 }
